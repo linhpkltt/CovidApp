df8e797e4d9cad5691152d9070094321fbd324bb
linhpkltt <phunglinh861999@gmail.com>
2021-06-23 11:55:30 +0700

    CovidUI-19  ver 1.0

---
+++ CovidApp-19/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CovidApp-19
+//
+//  Created by Khánh Linh on 22/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    @IBOutlet weak var RoundedCorner: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+//        redBox.backgroundColor = .red
+//        redBox.layer.cornerRadius = 25
+//        redBox.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMaxYCorner]
+//        view.addSubview(redBox)
+
+    }
+
+
+}
+
