a7442c8fc3580798bb67c9f2e0d5bf31e7e38fe9
linhpkltt <phunglinh861999@gmail.com>
2021-07-05 11:34:32 +0700

    add new home module

---
+++ CovidApp-19/Views/NewsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewsViewController.swift
+//  CovidApp-19
+//
+//  Created by Khánh Linh on 03/07/2021.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
